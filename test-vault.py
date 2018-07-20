80b93160a5ff9fb1beb739106e136d4ffe22f0e7
Sylvain Rabot <s.rabot@lectra.com>
2018-07-20 11:08:22 +0200

    pwet
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- test-vault.py
+++ test-vault.py
@@ -1,6 +1,9 @@
 from ansible.parsing.yaml.loader import AnsibleLoader
+from pprint import pprint
+from inspect import getmembers
 
 if __name__ == "__main__":
 	with open("pwet.yml", 'r') as stream:
-		loader = AnsibleLoader(stream, "pwet.yml")
-		print(loader.get_data())
+		loader = AnsibleLoader(stream, "pwet.yml", vault_secrets="azeaze")
+		pprint(getmembers(loader))
+		#print(loader.get_data())
